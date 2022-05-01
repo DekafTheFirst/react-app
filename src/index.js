0f4b95832a5b571073cde234e2b7c4843dad8166
Destiny Kefas <dekeji1@gmail.com>
2022-05-01 20:26:06 +0800

    homepage edited

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/react-app'>
     <App />
   </BrowserRouter>
 );
